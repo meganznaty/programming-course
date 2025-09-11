8cd6c380a5ce63831f35cee37a1dc2a82aef6228
Megan Znaty <meganznaty@gmail.com>
2025-09-11 10:47:13 -0400

    Add files via upload

---
+++ lab2megganznaty.py
@@ -0,0 +1,41 @@
+#getting input from user
+def calculate_height(h0, t):
+    g=9.8
+    return h0 - 0.5 * g* t* t
+
+height = float(input("Enter intial height"))
+time = float(input("Enter time"))
+calculate_height(height, time)
+print(f"Height of the ball at {time} is {calculate_height(height, time)}")
+
+height = float(input("Enter intial height"))
+time = float(input("Enter time"))
+calculate_height(height, time)
+print(f"Height of the ball at {time} is {calculate_height(height, time)}")
+
+height = float(input("Enter intial height"))
+time = float(input("Enter time"))
+calculate_height(height, time)
+print(f"Height of the ball at {time} is {calculate_height(height, time)}")
+     
+
+
+   
+def calculate_car_distance(t):
+    speed=20
+    return speed*t
+time = float(input("Enter time"))
+calculate_car_distance(time)
+print(f"The car will travel {calculate_car_distance(time)} in {time}")
+
+def calculate_car_distance(t):
+    speed=20
+    return speed*t
+time = float(input("Enter time"))
+print(f"The car will travel {calculate_car_distance(time)} in {time}")
+
+def calculate_car_distance(t):
+    speed=20
+    return speed*t
+time = float(input("Enter time"))
+print(f"The car will travel {calculate_car_distance(time)} in {time}")
